b1f78d49d7b4c8a56039c080735a33371539660b
Ronja Fagerdahl <ronjafagerdahl@gmail.com>
2025-01-23 21:31:50 +0100

    don`t know what I am doing

--- server.js
+++ server.js
@@ -1,30 +1,50 @@
-//define routes, import express
+import express from 'express';
+import axios from 'axios';
+import { engine } from 'express-handlebars';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-import express from "express";
-import { engine } from "express-handlebars";
-import { loadMovie, loadMovies } from "./src/movies.js"
-import fetch from "node-fetch";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
+const port = 5080;
 
-app.engine("handlebars", engine());
-app.set("view engine", "handlebars");
-app.set("views", "./templates");
+// Middleware for static files
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get("/", async (request, response) => {
-  const movies = await loadMovies();
-  response.render("home", { movies });
+//Templ engine
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+
+//Route for all movies
+app.get('/', async (req, res) => {
+  try {
+    const response = await axios.get('https://plankton-app-xhkom.ondigitalocean.app/api/movies');
+    const movies = response.data;
+
+    res.render('home', { movies });
+  } catch (error) {
+    res.status(500).send('Det gick inte att hämta filmer');
+  }
 });
 
-app.get("/movies/:movieId", async (request, response) => {
-  const movie = await loadMovie(request.params.movieId);
-  response.render("movie", { movie });
+// Route for one movie
+app.get('/movie/:id', async (req, res) => {
+  const { id } = req.params;
 
+  try {
+    const response = await axios.get(`https://plankton-app-xhkom.ondigitalocean.app/api/movies/${id}`);
+    const movie = response.data;
+
+    res.render('movie', { movie });
+  } catch (error) {
+    res.status(500).send('Det gick inte att hämta filmen');
+  }
 });
 
-app.use("/static", express.static("./static"));
-
-app.listen(5080);
-
-
-
+//serve
+app.listen(port, () => {
+  console.log(`Servern körs på http://localhost:${port}`);
+});
